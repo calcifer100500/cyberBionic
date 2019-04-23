d41765a69a2ffb5ec9381c98f771a2756a411a16
Julia Ivanova <jeivano91@gmail.com>
2019-04-23 22:18:23 +0300

    second task is done

--- 001/index.js
+++ 001/index.js
@@ -1,27 +1,69 @@
 /*task 01*/
-function Box(h,w,dep,mat) {
+function Box(h,w,dep) {
     this.Height = h;
     this.Width = w;
     this.Depth = dep;
-    this.Material = mat;
 
     this.volume = function() {
         return this.Height * this.Width * this.Depth;
     };
 }
-var box = new Box(20,40,30);
+let box = new Box(20,40,30);
 
-var div = document.createElement('div');
-div.className = 'first';
-document.body.appendChild(div);
-var title = document.createElement('h2');
-title.innerHTML = ('Task 01');
-div.appendChild(title);
-var p = document.createElement('P');
-p.innerHTML = ('box = '+ box.Height.toString() + ', ' + box.Width.toString() + ', ' + box.Depth.toString());
-div.appendChild(p);
-var pSecond = document.createElement('P');
-pSecond.innerHTML = ('box volume = ' + box.volume());
-div.appendChild(pSecond);
+function createDiv(divClass, title, inner, secondInner) {
+    let div = document.createElement('div');
+    div.className = divClass;
+    document.body.appendChild(div);
+    let heading = document.createElement('h2');
+    heading.innerHTML = title;
+    div.appendChild(heading);
+    let p = document.createElement('p');
+    p.innerHTML = inner;
+    div.appendChild(p);
+    let pSecond = document.createElement('p');
+    pSecond.innerHTML = secondInner;
+    div.appendChild(pSecond);
+}
+
+createDiv(
+    'first',
+    'Task 01',
+    'box = '+ box.Height.toString() + ', ' + box.Width.toString() + ', ' + box.Depth.toString(),
+    'box volume = ' + box.volume()
+);
 
 /*task 02*/
+function Cats(name, color, age, gender) {
+    this.Name = name;
+    this.Color = color;
+    this.Age = age;
+    this.Gender = gender;
+
+    this.sayMeow = function() {
+        return 'My name is ' + this.Name + '. ' + 'I am ' + this.Age + '. ' + 'I am ' + this.Color + '. ' + 'I am a ' + this.Gender + '.';
+    };
+}
+
+let cat1 = new Cats('Tom', 'grey', 10, 'boy');
+let cat2 = new Cats('Mia', 'black', 8, 'girl');
+let cat3 = new Cats('Jerry', 'white', 5, 'boy');
+
+createDiv(
+    'second',
+    'Task 02. First cat',
+    cat1.sayMeow(),
+    ''
+);
+createDiv(
+    'second',
+    'Task 02. Second cat',
+    cat2.sayMeow(),
+    ''
+);
+createDiv(
+    'second',
+    'Task 02. Third cat',
+    cat3.sayMeow(),
+    ''
+);
+
