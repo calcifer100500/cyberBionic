95da3e0321a072ac301e15957ab2be2f8d809a26
Julia Ivanova <jeivano91@gmail.com>
2019-04-23 17:04:10 +0300

    first task

--- 001/index.js
+++ 001/index.js
@@ -1,13 +1,27 @@
+/*task 01*/
 function Box(h,w,dep,mat) {
     this.Height = h;
     this.Width = w;
     this.Depth = dep;
     this.Material = mat;
+
     this.volume = function() {
         return this.Height * this.Width * this.Depth;
-    }
+    };
 }
 var box = new Box(20,40,30);
 
-console.log(box);
-console.log(box.volume());
+var div = document.createElement('div');
+div.className = 'first';
+document.body.appendChild(div);
+var title = document.createElement('h2');
+title.innerHTML = ('Task 01');
+div.appendChild(title);
+var p = document.createElement('P');
+p.innerHTML = ('box = '+ box.Height.toString() + ', ' + box.Width.toString() + ', ' + box.Depth.toString());
+div.appendChild(p);
+var pSecond = document.createElement('P');
+pSecond.innerHTML = ('box volume = ' + box.volume());
+div.appendChild(pSecond);
+
+/*task 02*/
